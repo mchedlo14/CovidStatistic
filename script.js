5a3c7c987e2cfbe30ec1d3d46a25f8fa543db33d
levan mchedlishvili <golocogo4@gmail.com>
2022-05-04 15:05:29 +0400

    fix input filed

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let input = document.getElementById('input');
+
+
+// input.addEventListener('focus',function(){
+//     input.value = ' '
+// })
+
 function getCovidStatistic(country){
     let quantity = document.querySelector('.quantity');
     let quantityRecovered = document.querySelector('.quantity-recovered');
     let death = document.querySelector('.quantity-dead');
+
+
     const options = {
         method: 'GET',
         headers: {
@@ -14,7 +23,9 @@ function getCovidStatistic(country){
         .then(response => response.json())
         .then(function(resData){
             resData.result.forEach(function(item){
-                if(item.country === country){
+                let countryUpperCase = country.toUpperCase()
+                console.log(resData)
+                if(item.country === country.charAt(0).toUpperCase() + country.slice(1) || item.country === countryUpperCase){
                     quantity.innerHTML = item.totalCases
                     quantityRecovered.innerHTML = item.totalRecovered
                     death.innerHTML = item.totalDeaths
@@ -24,13 +35,41 @@ function getCovidStatistic(country){
 }
 
 
-let input = document.getElementById('input');
-
-
 
 document.addEventListener('keypress',function(event){
     if(event.key === 'Enter'){
         let val = document.getElementById('input').value;
-        getCovidStatistic(val.charAt(0).toUpperCase() + val.slice(1))
+        getCovidStatistic(val)
     }
 })
+
+
+
+// function topTen(){
+//     let parentContainer = document.getElementById('topcountries');
+//     for(let i = 1;i <11;i++){
+//         let countryRows = `
+//         <div class="countrie-row">
+//             <p>1. Usa</p>
+//             <div class="container">
+//                 <h3>Total Cases</h3>
+//                 <p>83,123,122</p>
+//             </div>
+//             <div class="container">
+//                 <h3>New Cases</h3>
+//                 <p>83,123,122</p>
+//             </div>
+//             <div class="container">
+//                 <h3>Recovered</h3>
+//                 <p>83,123,122</p>
+//             </div>
+//             <div class="container">
+//                 <h3>Deaths</h3>
+//                 <p>83,123,122</p>
+//             </div>
+//         </div>
+//         `;
+//         parentContainer.innerHTML += countryRows
+//     }
+// }
+// topTen()
